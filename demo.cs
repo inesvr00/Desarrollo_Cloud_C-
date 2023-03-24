7a002250c8679cbb324a11d6632d6f9afd9903db
Inés Victoria Rodríguez <inesvictoria2000@gmail.com>
2023-03-24 11:09:53 +0100

    Prueba2

---
+++ demo.cs
@@ -0,0 +1,16 @@
+namespace Escuela {
+
+    class Materia {
+
+    }
+    
+}
+
+// Con partial escribo parcialmente la clase aquí y la completo de otro código
+namespace Universidad{
+    public partial class Profesor{
+        public int num1 = 0;
+    }
+    class Admin {}
+    class Persona {}
+}
